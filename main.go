2ac5abda62330f96bb5a0ead9376f7c38e8fe5e5
fourside <fourside@gmail.com>
2019-02-06 22:32:30 +0900

    file stat

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/xml"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -53,6 +56,16 @@ func feed(w http.ResponseWriter, r *http.Request) {
 			PubDate:     "baz",
 		},
 	}
+
+	mp4s, err := glob()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for _, mp4 := range mp4s {
+		fmt.Printf("%v (%d bytes) @%v\n", mp4.Name(), mp4.Size(), mp4.ModTime())
+	}
+
 	output, err := xml.MarshalIndent(rss, "", "  ")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -60,4 +73,20 @@ func feed(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/xml")
 	w.Write(output)
+}
+
+func glob() ([]os.FileInfo, error) {
+	files, err := filepath.Glob("public/*.mp4")
+	if err != nil {
+		return nil, err
+	}
+	var stats []os.FileInfo
+	for _, file := range files {
+		stat, err := os.Stat(file)
+		if err != nil {
+			return nil, err
+		}
+		stats = append(stats, stat)
+	}
+	return stats, nil
 }
