cc277da8a7e328f567b63734c6cd3e2382606fd9
fourside <fourside@gmail.com>
2019-02-16 14:13:58 +0900

    toggle menu open/close

--- frontend/src/menu.tsx
+++ frontend/src/menu.tsx
@@ -1,13 +1,45 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class Menu extends React.Component<{}, {}> {
+interface MenuState {
+  active: "" | "is-active"
+}
+export default class Menu extends React.Component<{}, MenuState> {
+  button: HTMLElement;
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: ""
+    };
+    document.addEventListener('click', this.handleExceptMenuClick.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleExceptMenuClick.bind(this));
+  }
+
+  handleMenuClick(e) {
+    this.button = e.target;
+    const active = this.state.active === "" ? "is-active" : "";
+    this.setState({
+      active: active
+    });
+  }
+
+  handleExceptMenuClick(e) {
+    if (this.button !== e.target) {
+      this.setState({
+        active: ""
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="dropdown is-right is-active">
+      <div className={this.state.active + " dropdown is-right"}>
 
         <div className="dropdown-trigger">
-          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={(e) => this.handleMenuClick(e)}>
             <span>menu</span>
             <span className="icon is-small">
               <i className="fas fa-bars" aria-hidden="true"></i>
