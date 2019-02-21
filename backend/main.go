2635fc8c8afff498e8aa4b833412ca7d061c8d9d
fourside <fourside@gmail.com>
2019-02-22 06:36:53 +0900

    refactor: split post api from main

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -46,93 +42,6 @@ func init() {
 	}
 }
 
-func postUrl(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	if r.Header.Get("Content-Type") != "application/json" {
-		w.WriteHeader(http.StatusNotAcceptable)
-		return
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	var post PostUrlRequest
-	err := decoder.Decode(&post)
-	if err != nil {
-		errorResponse(w, err)
-		return
-	}
-
-	_, err = url.ParseRequestURI(post.Url)
-	if err != nil {
-		errorResponse(w, err)
-		return
-	}
-
-	outputOption := fmt.Sprintf(publicDir+"/%s/%%(title)s.%%(ext)s", post.Category)
-	commandArgs := []string{
-		"-o",
-		outputOption,
-		"--write-thumbnail",
-		"--no-mtime",
-		"--newline",
-		"--no-overwrites",
-	}
-	if post.Subtitle {
-		commandArgs = append(commandArgs, "--write-auto-sub")
-	}
-	commandArgs = append(commandArgs, post.Url)
-	cmd := exec.Command(downloader, commandArgs...)
-
-	streamStdoutReader := func(r io.Reader, url string) {
-		title := getVideoTitle(url)
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			stdout := scanner.Text()
-			result := logPatttern.FindSubmatch([]byte(stdout))
-			if len(result) > 0 {
-				log.Printf("%s : %s, %s", title, string(result[1]), string(result[2]))
-			}
-		}
-	}
-	streamStderrReader := func(r io.Reader) {
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			log.Printf("[err] %s", scanner.Text())
-		}
-	}
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		errorResponse(w, err)
-		return
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		errorResponse(w, err)
-		return
-	}
-	go streamStdoutReader(stdout, post.Url)
-	go streamStderrReader(stderr)
-
-	err = cmd.Start()
-	if err != nil {
-		log.Printf("command failed: %v", err.Error())
-		errorResponse(w, err)
-		return
-	}
-}
-
-func getVideoTitle(url string) string {
-	commandArgs := []string{
-		"--get-title",
-		url,
-	}
-	out, _ := exec.Command(downloader, commandArgs...).CombinedOutput()
-	return strings.TrimRight(string(out), "\n")
-}
-
 func errorResponse(w http.ResponseWriter, error error) {
 	res, err := json.Marshal(ErrorResponse{
 		Message: error.Error(),
@@ -145,12 +54,6 @@ func errorResponse(w http.ResponseWriter, error error) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusBadRequest)
 	w.Write(res)
-}
-
-type PostUrlRequest struct {
-	Url      string `json:"url"`
-	Category string `json:"category"`
-	Subtitle bool   `json:"subtitle"`
 }
 
 type ErrorResponse struct {
