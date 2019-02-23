d51cf72122268a1b2ea4204667b20d324daf3afa
fourside <fourside@gmail.com>
2019-02-24 06:16:58 +0900

    refactor: move execute command

---
+++ backend/execute.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var (
+	logPatttern = regexp.MustCompile(`(\d{1,3}\.\d%).+?(ETA.+)`)
+)
+
+const (
+	downloader = "youtube-dl"
+)
+
+func download(url string, category string, subtitle bool) error {
+	outputOption := fmt.Sprintf(publicDir+"/%s/%%(title)s.%%(ext)s", category)
+	commandArgs := []string{
+		"-o",
+		outputOption,
+		"--write-thumbnail",
+		"--no-mtime",
+		"--newline",
+		"--no-overwrites",
+	}
+	if subtitle {
+		commandArgs = append(commandArgs, "--write-auto-sub")
+	}
+	commandArgs = append(commandArgs, url)
+	cmd := exec.Command(downloader, commandArgs...)
+
+	streamStdoutReader := func(r io.Reader, url string) {
+		title := getVideoTitle(url)
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			stdout := scanner.Text()
+			result := logPatttern.FindSubmatch([]byte(stdout))
+			if len(result) > 0 {
+				log.Printf("%s : %s, %s", title, string(result[1]), string(result[2]))
+			}
+		}
+	}
+	streamStderrReader := func(r io.Reader) {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			log.Printf("[err] %s", scanner.Text())
+		}
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	go streamStdoutReader(stdout, url)
+	go streamStderrReader(stderr)
+
+	err = cmd.Start()
+	return err
+}
+
+func getVideoTitle(url string) string {
+	commandArgs := []string{
+		"--get-title",
+		url,
+	}
+	out, _ := exec.Command(downloader, commandArgs...).CombinedOutput()
+	return strings.TrimRight(string(out), "\n")
+}
--- backend/post.go
+++ backend/post.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"os/exec"
-	"regexp"
-	"strings"
-)
-
-var (
-	logPatttern = regexp.MustCompile(`(\d{1,3}\.\d%).+?(ETA.+)`)
-)
-
-const (
-	downloader = "youtube-dl"
 )
 
 func postUrl(w http.ResponseWriter, r *http.Request) {
@@ -45,67 +31,12 @@ func postUrl(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	outputOption := fmt.Sprintf(publicDir+"/%s/%%(title)s.%%(ext)s", post.Category)
-	commandArgs := []string{
-		"-o",
-		outputOption,
-		"--write-thumbnail",
-		"--no-mtime",
-		"--newline",
-		"--no-overwrites",
-	}
-	if post.Subtitle {
-		commandArgs = append(commandArgs, "--write-auto-sub")
-	}
-	commandArgs = append(commandArgs, post.Url)
-	cmd := exec.Command(downloader, commandArgs...)
-
-	streamStdoutReader := func(r io.Reader, url string) {
-		title := getVideoTitle(url)
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			stdout := scanner.Text()
-			result := logPatttern.FindSubmatch([]byte(stdout))
-			if len(result) > 0 {
-				log.Printf("%s : %s, %s", title, string(result[1]), string(result[2]))
-			}
-		}
-	}
-	streamStderrReader := func(r io.Reader) {
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			log.Printf("[err] %s", scanner.Text())
-		}
-	}
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		errorResponse(w, err)
-		return
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		errorResponse(w, err)
-		return
-	}
-	go streamStdoutReader(stdout, post.Url)
-	go streamStderrReader(stderr)
-
-	err = cmd.Start()
+	err = download(post.Url, post.Category, post.Subtitle)
 	if err != nil {
 		log.Printf("command failed: %v", err.Error())
 		errorResponse(w, err)
 		return
 	}
-}
-
-func getVideoTitle(url string) string {
-	commandArgs := []string{
-		"--get-title",
-		url,
-	}
-	out, _ := exec.Command(downloader, commandArgs...).CombinedOutput()
-	return strings.TrimRight(string(out), "\n")
 }
 
 type PostUrlRequest struct {
