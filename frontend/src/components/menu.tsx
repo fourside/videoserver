607e5d60e8174bc66df6b9381e93ed71fb5ba6db
fourside <fourside@gmail.com>
2019-02-26 23:29:28 +0900

    use react hooks: menu component

--- frontend/src/components/menu.tsx
+++ frontend/src/components/menu.tsx
@@ -1,66 +1,56 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 interface MenuProps {
   toggleModal: () => void
 }
-interface MenuState {
-  active: "" | "is-active"
-}
 
-export default class Menu extends React.Component<MenuProps, MenuState> {
-  button: HTMLElement;
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: ""
+const Menu = ({toggleModal} :MenuProps) => {
+  const [active, setActive] = useState(false);
+  let button: HTMLElement;
+
+  useEffect(() => {
+    document.addEventListener('click', handleExceptMenuClick);
+    return () => {
+      document.removeEventListener('click', handleExceptMenuClick);
     };
-    document.addEventListener('click', this.handleExceptMenuClick.bind(this));
-  }
+  });
 
-  componentWillUnmount() :void {
-    document.removeEventListener('click', this.handleExceptMenuClick.bind(this));
-  }
+  const handleMenuClick = (e :any) :void => {
+    button = e.target;
+    setActive(!active);
+  };
 
-  handleMenuClick(e :any) :void {
-    this.button = e.target;
-    const active = this.state.active === "" ? "is-active" : "";
-    this.setState({
-      active: active
-    });
-  }
-
-  handleExceptMenuClick(e :any) :void {
-    if (this.state.active === "is-active" && this.button !== e.target) {
-      this.setState({
-        active: ""
-      });
+  const handleExceptMenuClick = (e :any) :void => {
+    if (active && button !== e.target) {
+      setActive(false);
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className={this.state.active + " dropdown is-right"}>
+  return (
+    <div className={(active ? "is-active" : "" ) + " dropdown is-right"}>
 
-        <div className="dropdown-trigger">
-          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={(e) => this.handleMenuClick(e)}>
-            <span>menu</span>
-            <span className="icon is-small">
-              <i className="fas fa-bars" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-
-        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <NavLink className="dropdown-item" exact to="/">Home</NavLink>
-            <a className="dropdown-item" onClick={() => this.props.toggleModal()} >Form</a>
-            <NavLink className="dropdown-item" exact to="/list">List</NavLink>
-            <a className="dropdown-item" href="/feed">RSS</a>
-          </div>
-        </div>
-
+      <div className="dropdown-trigger">
+        <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={handleMenuClick}>
+          <span>menu</span>
+          <span className="icon is-small">
+            <i className="fas fa-bars" aria-hidden="true"></i>
+          </span>
+        </button>
       </div>
-    );
-  }
+
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          <NavLink className="dropdown-item" exact to="/">Home</NavLink>
+          <a className="dropdown-item" onClick={toggleModal} >Form</a>
+          <NavLink className="dropdown-item" exact to="/list">List</NavLink>
+          <a className="dropdown-item" href="/feed">RSS</a>
+        </div>
+      </div>
+
+    </div>
+  );
 }
+
+export default Menu;
