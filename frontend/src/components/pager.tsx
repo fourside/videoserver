39ef5ded58cfd9ed00a5d043ff942820d453a91b
fourside <fourside@gmail.com>
2019-03-19 07:11:35 +0900

    pager emitted when many count

--- frontend/src/components/pager.tsx
+++ frontend/src/components/pager.tsx
@@ -7,34 +7,29 @@ interface PagerProps {
   onChange: (e) => void;
 }
 const itemPerPage = 15;
+const visibleItems = 3;
 
 const Pager = ({ total, currentPage, onChange }: PagerProps) => {
   const totalPage = Math.ceil(total / itemPerPage);
   const li = new Array<any>();
   for (let i = 0; i < totalPage; i++) {
     if (i === currentPage) {
-      li.push(
-        <li
-          key={i}
-          className="pagination-link is-current"
-          aria-label={`Page ${i}`}
-          aria-current="page">
-          <span>{i}</span>
-        </li>
-      );
-    } else {
-      li.push(
-        <li key={i}>
-          <a
-            className="pagination-link"
-            aria-label={`Goto page ${i}`}
-            onClick={() => onChange(i)}>
-            {i}
-          </a>
-        </li>
-      );
+      li.push(<CurrentPage index={i} key={i} />);
+    } else if (
+      i === 0 ||
+      i === totalPage - 1 ||
+      (currentPage < visibleItems && i <= visibleItems) ||
+      (currentPage > totalPage - visibleItems - 1 &&
+        i >= totalPage - visibleItems - 1) ||
+      i === currentPage + 1 ||
+      i === currentPage - 1
+    ) {
+      li.push(<Page index={i} onChange={onChange} key={i} />);
+    } else if (i === 1 || i === totalPage - 2) {
+      li.push(<Ellipsis />);
     }
   }
+
   return (
     <PagerNav
       className="pagination is-small"
@@ -44,6 +39,33 @@ const Pager = ({ total, currentPage, onChange }: PagerProps) => {
     </PagerNav>
   );
 };
+
+const CurrentPage = ({ index }) => (
+  <li
+    className="pagination-link is-current"
+    aria-label={`Page ${index}`}
+    aria-current="page">
+    <span>{index}</span>
+  </li>
+);
+
+const Page = ({ index, onChange }) => (
+  <li>
+    <a
+      className="pagination-link"
+      aria-label={`Goto page ${index}`}
+      onClick={() => onChange(index)}>
+      {index}
+    </a>
+  </li>
+);
+
+const Ellipsis = () => (
+  <li>
+    <span className="pagination-ellipsis">&hellip;</span>
+  </li>
+);
+
 const PagerNav = styled.nav`
   margin: 0.5em 0;
 `;
